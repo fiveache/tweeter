24963ebf0deb8a75e6c8a131527474c7e6831a08
David Lacho <david.lacho@gmail.com>
2018-11-07 12:22:20 -0800

    fixed bug where max character count is sometimes styled red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,19 @@
 $(document).ready(() => {
+  const maxLength = 140;
+  const counter = $('section.new-tweet textarea').siblings().filter('.counter');
+  counter.text(maxLength);
+
   const inputButton = $('.new-tweet form input');
   inputButton.attr('disabled', 'disabled');
+
   $('section.new-tweet textarea').on('input', (e) => {
-    const counter = $('section.new-tweet textarea').siblings().filter('.counter');
-    const counterVal = 140 - e.target.value.length;
+    const counterVal = maxLength - e.target.value.length;
     counter.text(counterVal);
     if (counterVal < 0) {
       counter.css('color', 'red');
       inputButton.attr('disabled', 'disabled');
-    } else if (counterVal === 140) {
+    } else if (counterVal === maxLength) {
+      counter.css('color', 'inherit');
       inputButton.attr('disabled', 'disabled');
     } else {
       counter.css('color', 'inherit');
